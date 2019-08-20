0f409437133eb1e6c2bfa6f1ed6742d2bb4e753c
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.home>
2019-08-19 21:49:00 -0400

    populates the main view controller

--- MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/ViewController.swift
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movie.count
+    }
+    
+   
+    
+    @IBOutlet var movieTableView: UITableView!
+    var movie = Movie.allMovies
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        movieTableView.dataSource = self
     }
+   
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath)
+        
+        cell.textLabel?.text = movie[indexPath.row].name
+        cell.detailTextLabel?.text = movie[indexPath.row].genre
+        cell.textLabel?.font = UIFont(name: "Helvetica", size: 20)
+        return cell
+    }
+    
 }
 
