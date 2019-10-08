e8c4fccea7f43f8b4a5a7605193609df48798352
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-10-08 11:50:39 -0400

    Completes Custom Delegate lab

--- MVC+Delegation-Review-Lab/StepperViewController.swift
+++ MVC+Delegation-Review-Lab/StepperViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 
 class StepperViewController: UIViewController {
     var sliderValueHolder = Float()
-  
     
-   // var oldValue: Float?
-var delegate:fontSizer?
+    
+    // var oldValue: Float?
+    var delegate:fontSizer?
     var currentValue: Float!
     
     @IBOutlet var fontSizerStepperOutlet: UIStepper!
@@ -22,26 +22,38 @@ var delegate:fontSizer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-fontSizerSliderOutlet.value = currentValue ?? 20
-        fontSizerStepperOutlet.value = Double(currentValue ?? 20)
-    previousFontSize.text = "Previous Font Size: \(String(describing: currentValue ?? 20))"
+        setCurrentValue()
     }
     
-    @IBAction func fontSizerSlider(_ sender: UISlider) {
+    private func setCurrentValue(){
+        fontSizerSliderOutlet.value = currentValue ?? 20
+        fontSizerStepperOutlet.value = Double(currentValue ?? 20)
+        previousFontSize.text = "Previous Font Size: \(String(describing: currentValue ?? 20))"
+    }
+    
+    private func handleSlider(value: Float){
         if delegate != nil{
-          sliderValueHolder = sender.value
+            sliderValueHolder = value
             delegate?.changeFontSize(size: sliderValueHolder)
             
-            previousFontSize.text = "Current Font Size: \(sender.value)"
-            fontSizerStepperOutlet.value = Double(sender.value)
+            previousFontSize.text = "Current Font Size: \(value)"
+            fontSizerStepperOutlet.value = Double(value)
         }
-        
+    }
+    
+    private func handleStepper(value:Double){
+        sliderValueHolder = Float(value)
+        delegate?.changeFontSize(size: sliderValueHolder)
+        fontSizerSliderOutlet.value = Float(value)
+        fontSizerStepperOutlet.value = Double(fontSizerSliderOutlet.value)
+        previousFontSize.text = "Current Font Size: \(value)"
+    }
+    
+    
+    @IBAction func fontSizerSlider(_ sender: UISlider) {
+        handleSlider(value: sender.value)
     }
     @IBAction func fontSizerStepper(_ sender: UIStepper) {
-        sliderValueHolder = Float(sender.value)
-        delegate?.changeFontSize(size: sliderValueHolder)
-        fontSizerSliderOutlet.value = Float(sender.value)
-        fontSizerStepperOutlet.value = Double(fontSizerSliderOutlet.value)
-        previousFontSize.text = "Current Font Size: \(sender.value)"
+        handleStepper(value: sender.value)
     }
 }
